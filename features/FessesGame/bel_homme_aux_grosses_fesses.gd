1ee66e390b7395f037a4e7789241e1e3fe452710
Crizomb <clementbart.45@gmail.com>
2026-07-24 21:03:11 +0200

    big ass game new graphics (not finished)

--- features/FessesGame/bel_homme_aux_grosses_fesses.gd
+++ features/FessesGame/bel_homme_aux_grosses_fesses.gd
@@ -1,28 +1,24 @@
 class_name BigAssMan
 extends Node2D
 
-@onready var idle: Sprite2D = $Idle
-@onready var fesse_droite: Sprite2D = $FesseDroite
-@onready var fesse_gauche: Sprite2D = $FesseGauche
+@onready var body: Sprite2D = $Body
+@onready var ass: Sprite2D = $Ass
+
+var base_transform: Transform3D
 
 # TODO mettre les beaux dessins de maxime + changer la logique de contracte pour l'animation des fesses
 func _ready() -> void:
+	base_transform = ass.global_transform
 	decontracte()
 	
 func decontracte():
-	idle.visible = true
-	fesse_droite.visible = false
-	fesse_gauche.visible = false
+	pass
 
 func contracte_gauche():
-	idle.visible = false
-	fesse_droite.visible = false
-	fesse_gauche.visible = true
+	pass
 	
 func contracte_droite():
-	idle.visible = false
-	fesse_droite.visible = true
-	fesse_gauche.visible = false
+	pass
 
 func fesse_animation(player: LocalPlayer) -> void:
 	if player.input.action_1_just_pressed:
